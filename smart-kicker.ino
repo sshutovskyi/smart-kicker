0fb8701fc990bd0825d540a06ec118af3bcd9623
Sergii Shutovskyi <s.shutovskyi@osram.com>
2019-07-25 16:55:42 +0200

    prepare hw for demo

--- smart-kicker.ino
+++ smart-kicker.ino
@@ -1,35 +1,35 @@
 /*
- * 
- * Tutorial links
- * http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/arduino-hc-sr501-motion-sensor-tutorial
- * https://randomnerdtutorials.com/security-access-using-mfrc522-rfid-reader-with-arduino
- 
- * ----------------------------------------
- * | HC-SR501 Motion Detector | Arduino   |
- * ----------------------------------------
- * | RL (output)              | Digital 8 |
- * | GND                      | GND       |
- * | VCC                      | 5V        |
- * ----------------------------------------
- * 
- * -----------------------------------
- * | RC522 RFID Reader | Arduino     |
- * -----------------------------------
- * | SDA               | Digital 10  |
- * | SCK               | Digital 13  |
- * | MOSI              | Digital 11  |
- * | MISO              | Digital 12  |
- * | IRQ               | Unconnected |
- * | GND               | GND         |
- * | RST               | Digital 9   |
- * | 3.3V              | 3.3V        |
- * -----------------------------------
- *
- */
+
+   Tutorial links
+   http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/arduino-hc-sr501-motion-sensor-tutorial
+   https://randomnerdtutorials.com/security-access-using-mfrc522-rfid-reader-with-arduino
+
+   ----------------------------------------
+   | HC-SR501 Motion Detector | Arduino   |
+   ----------------------------------------
+   | RL (output)              | Digital 8 |
+   | GND                      | GND       |
+   | VCC                      | 5V        |
+   ----------------------------------------
+
+   -----------------------------------
+   | RC522 RFID Reader | Arduino     |
+   -----------------------------------
+   | SDA               | Digital 10  |
+   | SCK               | Digital 13  |
+   | MOSI              | Digital 11  |
+   | MISO              | Digital 12  |
+   | IRQ               | Unconnected |
+   | GND               | GND         |
+   | RST               | Digital 9   |
+   | 3.3V              | 3.3V        |
+   -----------------------------------
+
+*/
 
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
@@ -37,58 +37,42 @@ int pirPin = 8; // Input for HC-S501
 int pirValue; // Place to store read PIR Value
 int oldPirValue = 0;
 String content;
-String oldContent = "";
-unsigned long old_time;
-unsigned long new_time;
+
+unsigned long lastRead = 0;
 
 unsigned long period = 1000;
 
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
   pinMode(pirPin, INPUT);
-  new_time = millis();
 }
-void loop() 
+
+void loop()
 {
   pirValue = digitalRead(pirPin);
   if (pirValue != oldPirValue) {
-    // Change the presence sensor number when uploading to a specific device
-    content = "{'presence': 1, 'value': ";
-    content.concat(String(pirValue));
-    content.concat("'}");
     oldPirValue = pirValue;
+    if (pirValue == 1) {
+      content = "{\"type\":\"presence\",\"value\":1}";
+      Serial.print(content);
+    }
+  }
+
+  if (millis() - lastRead > 5000 && mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+  {
+    lastRead = millis();
+    // Change the rfid reader number when uploading to a specific device
+    content = "{\"type\":\"rfid\",\"card\":\"";
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      content.concat(String(mfrc522.uid.uidByte[i]));
+    }
+    content.concat("\"}");
     Serial.print(content);
   }
-  
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
-  }
-  // Change the rfid reader number when uploading to a specific device
-  content = "{'rfid': 1, 'card': '";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     content.concat(String(mfrc522.uid.uidByte[i]));
-  }
-  content.concat("'}");
-  if (content != oldContent) {
-    Serial.print(content);
-    oldContent = content;
-  }
-  new_time = millis();
-  if (new_time - old_time > 5000) {
-    oldContent = "";
-  }
-  old_time = new_time;
+  delay(100);
 }
--- smart-kicker.py
+++ smart-kicker.py
@@ -1,57 +1,241 @@
 #!/usr/bin/env python
 
+import ssl
 import serial
 import pygame
 import os
+import json
+import time
+import paho.mqtt.client as mqtt
+import random
+import strict_rfc3339
+
+def get_datetime():
+    return strict_rfc3339.timestamp_to_rfc3339_utcoffset(int(time.time()))
+
+WHITE = (255, 255, 255)
+class Screen:
+    def __init__(self):
+        os.putenv('SDL_FBDEV', '/dev/fb0')
+        pygame.init()
+        self.lcd = pygame.display.set_mode((900, 700))
+        self.lcd.fill((255, 0, 0))
+        pygame.mouse.set_visible(False)
+        pygame.display.update()
+        self.font_big = pygame.font.Font(None, 60)
+        self.lcd.fill((0, 0, 0))
+        text_surface = self.font_big.render('SMART KICKER', True, WHITE)
+        rect = text_surface.get_rect(center=(240, 160))
+        self.lcd.blit(text_surface, rect)
+        pygame.display.update()
+
+    def render(self, text):
+        self.lcd.fill((0, 0, 50))
+        text_surface = self.font_big.render(text, True, WHITE)
+        rect = text_surface.get_rect(center=(500, 300))
+        self.lcd.blit(text_surface, rect)
+        pygame.display.update()
+
+class Sensor:
+    def __init__(self, port):
+        self.serial = serial.Serial(port, 9600)
+        self.serial.flushInput()
+        self.buffer = ''
+
+    def read(self):
+        if self.serial.inWaiting() > 0 :
+            char = self.serial.read(1)
+            self.buffer += char
+            if '}' in char :
+                event = json.loads(self.buffer)
+                self.buffer = ''
+                return event
+
+        return None
+
+class Player:
+    def __init__(self, id, name, key):
+        self.id = id
+        self.name = name
+        self.key = key
+    def __eq__(self, other):
+        return self.id == other.id
+
+class Table:
+    def __init__(self, sides):
+        self.state = 'waiting'
+        self.registered_players = {}
+        self.sides = sides
+
+        self.screen = None
+        self.client = None
+
+    def set_registered_players(self, players):
+        self.registered_players = {}
+        for player in players:
+            self.registered_players[player.key] = player
+
+    def reset(self):
+        self.state = 'waiting'
+        for side in self.sides:
+            side.reset()
+        self.screen.render('Scan RFID cards to start')
+
+    def tick(self):
+        for side in self.sides:
+            event = side.get_event()
+            if event == None:
+                continue
+
+            eventType = event.get('type', None)
+            if self.state == 'waiting' and eventType == 'rfid':
+                self.add_player(side, event['card'])
+            elif self.state == 'running' and eventType == 'presence':
+                self.register_goal(side)
+
+    def add_player(self, target, key):
+        if len(target.players) >= 2:
+            self.screen.render('Team ' + target.name + ' is full')
+            return
+
+        if key not in self.registered_players:
+            self.screen.render('Unknown player')
+            return
+
+        player = self.registered_players[key]
+        for side in self.sides:
+            if side != target and side.players.count(player) > 0:
+                self.screen.render('Already registered')
+                return
+
+        target.players.append(player)
+        self.screen.render('Player ' + player.name + ' joined team ' + target.name)
+
+        teams_full = True
+        for side in self.sides:
+            if len(side.players) < 2:
+                teams_full = False
+
+        if teams_full:
+            self.start_game()
+
+    def start_game(self):
+        self.state = 'running'
+        self.started_at = get_datetime()
+        self.render_score()
+
+    def register_goal(self, receiver):
+        for side in self.sides:
+            if side == receiver:
+                continue
+            side.score += 1
+            if side.score >= 10:
+                self.end_game(side)
+            else:
+                self.render_score()
+
+    def end_game(self, winner):
+        self.screen.render('Team ' + winner.name + ' won')
+        message = {
+            'type': 'matchCompleted',
+            'value': {
+                'startedAt': self.started_at,
+                'finishedAt': get_datetime(),
+                'participants': []
+            }
+        }
+        for side in self.sides:
+            message['value']['participants'].append({
+                'score': side.score,
+                'players': [player.id for player in side.players]
+            })
+
+        self.client.publish('event-ingest', json.dumps(message))
+
+        time.sleep(5)
+        self.reset()
+
+    def render_score(self):
+        scores = [str(side.score) for side in self.sides]
+        self.screen.render(':'.join(scores))
+
+class Side:
+    def __init__(self, name, sensor):
+        self.name = name
+        self.score = 0
+        self.players = []
+        self.sensor = sensor
+
+    def get_event(self):
+        return self.sensor.read()
+
+    def reset(self):
+        self.score = 0
+        self.players = []
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    result = client.subscribe("devices/f0548d61-7307-4f3e-96df-2fdbd7c4e979/configuration")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, message):
+    parsedMessage = json.loads(message.payload)
+    if 'configuration' in parsedMessage and 'players' in parsedMessage['configuration']:
+        with open('players.json', 'w') as outfile:
+            json.dump(parsedMessage['configuration']['players'], outfile)
+        load_players(parsedMessage['configuration']['players'])
+
+def load_players(items):
+    players = []
+    for item in items:
+        players.append(Player(item['id'], item['name'], item['key']))
+    table.set_registered_players(players)
 
 # Depends on which Arduino device is connected to USB, ls /dev/tty* will show the list
-port1 = "/dev/ttyACM0"
-port2 = "/dev/ttyACM1"
+class FakeScreen:
+    def render(self, text):
+        print("PRINT: " + text)
+class FakeSensor:
+    def read(self):
+        return random.choice([
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            { 'type': 'presence' },
+            { 'type': 'presence' },
+            { 'type': 'rfid', 'card': '1' },
+            { 'type': 'rfid', 'card': '2' },
+            { 'type': 'rfid', 'card': '3' },
+            { 'type': 'rfid', 'card': '4' },
+            { 'type': 'rfid', 'card': 'fake' },
+        ])
+table = Table([
+    Side('1', Sensor("/dev/ttyACM0")),
+    Side('2', Sensor("/dev/ttyACM1")),
+])
 
-s1 = serial.Serial(port1, 9600)
-s1.flushInput()
+with open('players.json') as json_file:
+    items = json.load(json_file)
+    load_players(items)
 
-s2 = serial.Serial(port2, 9600)
-s2.flushInput()
+client = mqtt.Client('smartkicker')
+client.on_connect = on_connect
+client.on_message = on_message
+ssl_context = ssl.create_default_context()
+ssl_context.load_verify_locations(cafile = "olt_ca.pem")
+ssl_context.load_cert_chain(certfile = 'device_cert.pem', keyfile = 'device_key.pem')
+client.tls_set_context(context = ssl_context)
+client.connect("mqtt.lightelligence.io", port = 8883)
+client.loop_start()
 
-inputValue1 = ""
-inputValue2 = ""
-character1 = ""
-character2 = ""
-WHITE = (255, 255, 255)
-os.putenv('SDL_FBDEV', '/dev/fb0')
-pygame.init()
-lcd = pygame.display.set_mode((900, 700))
-lcd.fill((255, 0, 0))
-pygame.mouse.set_visible(False)
-pygame.display.update()
-font_big = pygame.font.Font(None, 60)
-lcd.fill((0, 0, 0))
-text_surface = font_big.render('Hatem', True, WHITE)
-rect = text_surface.get_rect(center=(240, 160))
-lcd.blit(text_surface, rect)
-pygame.display.update()
+table.screen = Screen()
+table.client = client
 
 while True:
-    if s1.inWaiting() > 0 :
-        character1 = s1.read(1)
-        inputValue1 += character1
-        if '}' in character1 :
-            print(inputValue1)
-            lcd.fill((0, 0, 50))
-            text_surface = font_big.render(inputValue1.strip(), True, WHITE)
-            rect = text_surface.get_rect(center=(500, 300))
-            lcd.blit(text_surface, rect)
-            pygame.display.update()
-            inputValue1 = ""
-    if s2.inWaiting() > 0 :
-        character2 = s2.read(1)
-        inputValue2 += character2
-        if '}' in character2 :
-            print(inputValue2)
-            lcd.fill((0, 0, 50))
-            text_surface = font_big.render(inputValue2.strip(), True, WHITE)
-            rect = text_surface.get_rect(center=(500, 300))
-            lcd.blit(text_surface, rect)
-            pygame.display.update()
-            inputValue2 = ""
+    table.tick()
